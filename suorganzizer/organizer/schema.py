1d2a437d7581828fb3a17bc5cbff8c854c97139f
Daniel Nguyen <danieltn91@gmail.com>
2017-04-06 00:43:17 -0700

    Update schema.py

--- suorganzizer/organizer/schema.py
+++ suorganzizer/organizer/schema.py
@@ -8,14 +8,17 @@ from organizer.models import NewsLink as NewsLinkModel
 class Tag(DjangoObjectType):
     class Meta:
         model = TagModel
+Tag.Connection = connection_for_type(Tag)
 
 class Startup(DjangoObjectType):
     class Meta:
         model = StartupModel
+Startup.Connection = connection_for_type(Startup)
 
 class NewsLink(DjangoObjectType):
     class Meta:
         model = NewsLinkModel
+Newslink.Connection = connection_for_type(Newslink)
 
 class Query(graphene.AbstractType):
     
@@ -23,6 +26,8 @@ class Query(graphene.AbstractType):
     all_startups = graphene.List(Startup)
     all_newslinks = graphene.List(NewsLink)
 
+    def connection_for_type(_type):
+        
     def resolve_all_tags(self, args, context, info):
         return TagModel.objects.all()
 
@@ -31,3 +36,16 @@ class Query(graphene.AbstractType):
 
     def resolve_all_newslinks(self, args, context, info):
         return NewsLinkModel.objects.all()
+ 
+
+class Connection(graphene.Connection):
+       total_count = graphene.Int()
+
+       class Meta:
+           name = _type._meta.name + 'Connection'
+           node = _type
+
+       def resolve_total_count(self, args, context, info):
+           return self.length
+
+   return Connection   
